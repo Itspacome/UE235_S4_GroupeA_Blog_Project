ae04ead3e0913658ea91ade27a37ed14d7f6f776
MartialEscudero <martial.escudero@gmail.com>
2021-01-29 23:15:27 +0100

    add front accueil +  add asset

--- assets/app.js
+++ assets/app.js
@@ -11,14 +11,19 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+// app.js
+
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
-require('@fortawesome/fontawesome-free/css/all.min.css');
-require('@fortawesome/fontawesome-free/js/all.js');
-
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
