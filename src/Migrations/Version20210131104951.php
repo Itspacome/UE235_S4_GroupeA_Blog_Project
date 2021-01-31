ec9d1ef411e8b13dacd4a86d7cb62165c4f27aff
Itspacome <contact@pacomeperrichot.com>
2021-01-31 14:51:34 +0000

    Migration cleanup and new

--- src/Migrations/Version20210131104951.php
+++ src/Migrations/Version20210131104951.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210130123326 extends AbstractMigration
+final class Version20210131104951 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -28,10 +28,11 @@ final class Version20210130123326 extends AbstractMigration
         $this->addSql('DROP INDEX IDX_23A0E6612469DE2');
         $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, category_id, title, content FROM article');
         $this->addSql('DROP TABLE article');
-        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER DEFAULT NULL, title VARCHAR(255) NOT NULL COLLATE BINARY, content CLOB NOT NULL COLLATE BINARY, CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER DEFAULT NULL, author_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL COLLATE BINARY, content CLOB NOT NULL COLLATE BINARY, CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_23A0E66F675F31B FOREIGN KEY (author_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('INSERT INTO article (id, category_id, title, content) SELECT id, category_id, title, content FROM __temp__article');
         $this->addSql('DROP TABLE __temp__article');
         $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)');
+        $this->addSql('CREATE INDEX IDX_23A0E66F675F31B ON article (author_id)');
     }
 
     public function down(Schema $schema) : void
@@ -41,6 +42,7 @@ final class Version20210130123326 extends AbstractMigration
 
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP INDEX IDX_23A0E6612469DE2');
+        $this->addSql('DROP INDEX IDX_23A0E66F675F31B');
         $this->addSql('CREATE TEMPORARY TABLE __temp__article AS SELECT id, category_id, title, content FROM article');
         $this->addSql('DROP TABLE article');
         $this->addSql('CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER DEFAULT NULL, title VARCHAR(255) NOT NULL, content CLOB NOT NULL)');
