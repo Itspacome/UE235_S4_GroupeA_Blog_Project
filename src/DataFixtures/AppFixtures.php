25cc1c85e56c1c54408b9ecb9037fbcab9f0cecc
Itspacome <contact@pacomeperrichot.com>
2021-01-31 14:44:05 +0000

    Integration of Users in AppFixtures
    and modification of fixtures articles

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,80 +6,53 @@ use App\Entity\Article;
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        //Gestion utilisateurs
+        $users = [];
+
+        $user = new User();
+        $user->setPseudo('John');
+        $user->setPassword($this->encoder->encodePassword($user,'mdp'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setPseudo('Paul');
+        $user->setPassword($this->encoder->encodePassword($user,'mdp'));
+        $manager->persist($user);
+ 
+        $users[] = $user;
+
+        //Gestion des catégories
         $category = new Category();
+
         $category->setName("Catégorie 1");
         $manager->persist($category);
 
-        $article = new Article();
-        $article->setTitle('Article 1');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
+        //Gestion des articles
+        for ($i=1; $i <= 24 ; $i++) { 
+            $article = new Article();
 
-        $article = new Article();
-        $article->setTitle('Article 2');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
+            $user = $users[mt_rand(0, count($users) - 1)];
 
-        $article = new Article();
-        $article->setTitle('Article 3');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
+            $article->setTitle('Article ' . $i);
+            $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
+            $article->setCategory($category);
+            $article->setAuthor($user);
 
-        $article = new Article();
-        $article->setTitle('Article 4');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
-
-        $article = new Article();
-        $article->setTitle('Article 5');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
-
-        $article = new Article();
-        $article->setTitle('Article 6');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
-
-        $article = new Article();
-        $article->setTitle('Article 7');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
-
-        $article = new Article();
-        $article->setTitle('Article 1');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
-
-        $article = new Article();
-        $article->setTitle('Article 8');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
-
-        $article = new Article();
-        $article->setTitle('Article 9');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
-
-        $article = new Article();
-        $article->setTitle('Article 10');
-        $article->setContent('Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.');
-        $article->setCategory($category);
-        $manager->persist($article);
+            $manager->persist($article);
+        }
 
         $manager->flush();
     }
