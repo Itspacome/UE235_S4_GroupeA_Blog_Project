fa32c956c3f45d982bcb45b7db4461f098811447
MartialEscudero <martial.escudero@gmail.com>
2021-01-31 16:05:52 +0100

    all front-end

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    setTimeout(showPage, 1000);
+    
+    function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+    }
+
+    ///////////////////////////////////////////////////////////// 
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= 10) {
+            $('.topBtn').addClass('hide');
+        } else {
+            $('.topBtn').removeClass('hide');
+        }
+    })
+    $('.topBtn').on('click', function() {
+        $('html').animate({scrollTop:0});
+    })
+
 });
